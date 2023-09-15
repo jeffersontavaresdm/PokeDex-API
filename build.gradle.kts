8a5ad0f83086f76ec7be7488524994a959f68018
jeffersontdm <jeffersontavares1611@gmail.com>
2023-09-14 21:04:56 -0300

    add connection with postgres

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
   implementation("org.springframework.boot:spring-boot-starter-validation")
+  implementation("org.postgresql:postgresql:42.2.27")
   implementation("org.springframework.boot:spring-boot-starter-test") {
     exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
   }
